3f3300e738f5b42fb44924bad2b860ce5ffd0137
Madhur Gupta <2020kuec2014@iiitkota.ac.in>
2023-11-08 01:15:42 +0530

    Merged

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -2,19 +2,11 @@ import React from "react";
 import { Link } from 'react-router-dom';
 export default function Home() {
     const options = [
-<<<<<<< HEAD
         { Heading: "Sort", color: "bg-violet-900" },
         { Heading: "Sudoku", color: "bg-blue-900" },
         { Heading: "PathFinding", color: "bg-green-900" },
         { Heading: "CellularAutomata", color: "bg-yellow-900" },
         { Heading: "GameOfLife", color: "bg-red-900" }];
-=======
-        ["Sort", "bg-yellow-600"],
-        ["Sudoku", "bg-orange-600"],
-        ["PathFinding", "bg-blue-900"],
-        ["CellularAutomata", "bg-pink-900"],
-        ["GameOfLife", "bg-pink-900"]];
->>>>>>> 4d79dda404a740062c8687ac80b4345836cd8480
     return (
         // <div class></div>
         <div className="flex w-full h-screen">
